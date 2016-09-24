00914b925dfaac276e8d8cd837e8b14096058dac
Matthew Behrens <mattbehrens96@gmail.com>
2016-09-24 13:49:06 -0500

    list displays items and can add items

--- www/js/list_controller.js
+++ www/js/list_controller.js
@@ -1,19 +1,44 @@
 var app = angular.module("list.controller",[])
-app.controller("listController", function($scope){
+app.controller("listController", function($scope, $ionicModal, $timeout){
 
-  $scope.items = [];
+	$scope.items = [];
+	$scope.item = "";
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/add_item.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openItemForm = function() {
+    $scope.modal.show();
+  };
+
+  $scope.closeItemForm = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.submitItemForm = function() {
+    console.log('Adding Item', $scope.item);
+    var input = this.item
+    if(input.length > 0){
+    	$scope.addItem(input);
+  	}
+    $timeout(function() {
+      $scope.closeItemForm();
+    }, 100);
+  };
 
   $scope.data = {
   	showDelete: false
-  }
+  };
 
-  $scope.addItem = function (itemAmount, itemName) {
-   $scope.items.push({
-      name: "Matt!"
-   });
- };
+  $scope.addItem = function (item) {
+  	$scope.items.push(item);
+	};
   
- $scope.removeItem = function (index) {
-   $scope.items.splice(index, 1);
- };
+	$scope.removeItem = function (index) {
+  	$scope.items.splice(index, 1);
+	};
 })
