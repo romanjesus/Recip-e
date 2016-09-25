23288529b23caf1879536323eb002880d822c1b8
William Huebler <email": null>
2016-09-25 17:33:29 -0500

    Created a theoretical GET route in the ionic controller

--- www/js/favorites_controller.js
+++ www/js/favorites_controller.js
@@ -2,4 +2,16 @@ angular.module("favorites.controller",[])
 
 .controller("favoritesController", function($scope, $http){
 
+  $scope.getFavorites = function() {
+    $http.get("http://recip-e.herokuapp.com/api/user/" + user.id + "/favorite_recipes")
+    .success(function(data){
+      // alert("SUCCESS!");
+      console.log(data)
+      $scope.favorite_recipes = data;
+    })
+    .error(function(data) {
+      alert("ERROR");
+    })
+  }
+
 })
