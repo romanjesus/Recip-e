11545d337f3362000347eb98af32e0e003ff9836
William Huebler <email": null>
2016-09-25 16:30:30 -0500

    Create a controller for the Favorite Recipes

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'list.controller', 'recipe.controller', 'userSessions.controller','user.controller', 'ng-token-auth'])
+angular.module('starter', ['ionic', 'starter.controllers', 'list.controller', 'recipe.controller', 'userSessions.controller','user.controller', 'favorites.controller', 'ng-token-auth'])
 
 .run(function($ionicPlatform, $rootScope, $location) {
   $ionicPlatform.ready(function() {
