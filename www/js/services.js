f86928c21bde4c6eca4b9af2df135b6732d7ef3d
William Huebler <email": null>
2016-09-28 13:33:09 -0500

    Create a factory to hit and stash the favorites

--- www/js/services.js
+++ www/js/services.js
@@ -9,3 +9,18 @@ angular.module("services", [])
 
   return { returnedRecipes: this.returnedRecipes, setReturned: setReturned };
 }])
+
+.factory("favorites", ['$http', function($http) {
+  var getFavorites = function() {
+    if(!this.favorites) {
+      $http.get("https://recip-e.herokuapp.com/api/favorite_recipes")
+      .success(function(data){
+        console.log(data);
+        debugger
+        this.favorites = data;
+      });
+    }
+  }
+
+  return { favorites: this.favorites, getFavorites: getFavorites };
+}])
